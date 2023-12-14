956d0360c6ceb0d43bb22e5089a22bfe762de9e2
schmidko <einfach.schmidt@gmail.com>
2023-12-14 20:12:00 +0100

    finished day3a

--- day03a/src/main.rs
+++ day03a/src/main.rs
@@ -3,35 +3,57 @@ use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
 fn main() {
-    let filename: &str = "test.txt";
+    let filename: &str = "data.txt";
     let lines: Vec<Vec<char>> = read_file(filename).unwrap();
     let mut numbers: Vec<String> = Vec::new();
     let mut number = String::new();
+    let mut flag: bool = false;
     for (y, line) in lines.iter().enumerate() {
         for (x, c) in line.iter().enumerate() {
             if c.is_numeric() {
                 number.push(*c);
-
-                if !lines[y][x+1].is_numeric() {
-                    numbers.push(number.clone());
-                    number = String::new();
+                let has_sign = check_special_sign(x, y, lines.clone());
+                if has_sign {
+                    flag = true;
                 }
-                
+                if x+1 >= line.len() || !lines[y][x+1].is_numeric() {
+                    if flag == true {
+                        numbers.push(number.clone());
+                    }
+                    number = String::new();
+                    flag = false;
+                }
             }
-            
         }
     }
 
     println!("{:?}", numbers);
+
+    let mut sum = 0;
+    for n in numbers {
+        sum = sum + n.parse::<i32>().unwrap();
+    }
+
+    println!("{}", sum)
 }
 
-fn check_special_sign(x: usize, y:usize, matrix: Vec<Vec<char>>) -> bool {
-    const RADIX: u32 = 10;
-    if matrix[y][x].to_digit(RADIX) > Some(3) {
-        true
-    } else {
-        false
+fn check_special_sign(x: usize, y: usize, matrix: Vec<Vec<char>>) -> bool {
+    let mut has_sign = false;
+   
+    let pos: [[isize; 2]; 8] = [[-1, 1], [0, 1], [1, 1], [-1, -1], [0, -1], [1, -1], [-1, 0], [1, 0]];
+    for coords in pos {
+        let xpos = x.saturating_add_signed(coords[0]);
+        let ypos = y.saturating_add_signed(coords[1]);
+        
+        if ypos >= matrix.len() || xpos >= matrix[0].len() {
+            continue;
+        }
+        if !matrix[ypos][xpos].is_numeric() && matrix[ypos][xpos] != '.' {
+            has_sign = true;
+        }
     }
+
+    has_sign
 }
 
 fn read_file(filepath: &str) -> Result<Vec<Vec<char>>, Box<dyn std::error::Error>> {
